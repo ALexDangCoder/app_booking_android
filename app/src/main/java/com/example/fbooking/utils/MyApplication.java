3ed75da92079ee9117e12327058038bce66728c1
nhat <nguyennhat622001@gmail.com>
2022-01-17 09:55:22 +0700

    Code thông báo (Có thể thông báo kể cả khi chạy app hoặc không)

---
+++ app/src/main/java/com/example/fbooking/utils/MyApplication.java
@@ -0,0 +1,29 @@
+package com.example.fbooking.utils;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class MyApplication extends Application {
+
+    public static final String CHANNEL_ID = "push_notification_id";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createChanelNotification();
+    }
+
+    private void createChanelNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "PushNotification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+}
--- app/src/main/java/com/example/fbooking/utils/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/example/fbooking/utils/MyFirebaseInstanceIDService.java
@@ -1,5 +1,9 @@
 package com.example.fbooking.utils;
 
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -7,6 +11,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
+import com.example.fbooking.MainActivity;
 import com.example.fbooking.R;
 import com.example.fbooking.history.HistoryActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -27,6 +32,37 @@ public class MyFirebaseInstanceIDService extends FirebaseMessagingService {
         super.onMessageReceived(remoteMessage);
         Log.d("TAG", "From: " + remoteMessage.getFrom());
         Log.d("TAG", "Message data payload: " + remoteMessage.getData());
+
+        //Ham viet them
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        if (notification == null) {
+            return;
+        }
+
+        String strTitle = notification.getTitle();
+        String strBody = notification.getBody();
+
+        sendNotification(strTitle, strBody);
+    }
+
+    //Ham viet them
+    private void sendNotification(String strTitle, String strBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,
+                MyApplication.CHANNEL_ID)
+                .setContentTitle(strTitle)
+                .setContentText(strBody)
+                .setSmallIcon(R.drawable.logo_mini)
+                .setContentIntent(pendingIntent);
+
+        Notification notification = notificationBuilder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.notify(1, notification);
+        }
     }
 
     //Ham test
